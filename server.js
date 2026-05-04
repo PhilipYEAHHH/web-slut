d1a5c5c7057cfc525119fb56b0174202dff19cfe
Philip Johansson <philip.hedera@gmail.com>
2026-05-04 12:25:20 +0200

    started postman

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(3000, ( ) => {
-  console.log(`Server is running on port ${PORT}`);
+app.post("/tasks", (req, res) => {
+  const action = req.body.action;
+  res.send(`Received action: ${action}`);
 });
